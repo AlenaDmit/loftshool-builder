0fd46b3047630c5a9dbb1ff0ee43cee5cb32fff2
AlenaDmit <dmitalyona@yandex.ru>
2016-07-02 01:16:58 +0300

    index and myWorks pages added. TODO: alot...

--- gulp/paths/template.js
+++ gulp/paths/template.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = [
-  './source/template/index.jade'
+	'./source/template/myworks.jade',
+ 	'./source/template/index.jade',
+ 	'./source/template/about.jade'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,69 @@
-(function() {
-  'use strict';
+function mainBlockApp() {	//main block vertical appearence
+	$(".main__block").css({
+		transform: 'rotateX(0deg)',
+		transformOrigin: 'top',
+		opacity: '1',
+		transition: 'opacity .5s, transform .5s'
+	});
+}
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+function flipInit(argument) { // main block flip 
+	$(".main__button").click(function(event) {
+
+		$(".main__block_wrap").css('perspective', 'none');
+		
+		$(this).fadeOut('300', function() {
+			
+		});
+
+		$(".main__block").css({
+			transition: 'transform .3s',
+			transform: 'rotateY(90deg)'
+		});
+
+		setTimeout(function(){
+			$(".main__block").css({
+				display: 'none'
+			});
+			$(".form__login").css({
+				display: 'block'
+			});
+		}, 300);
+
+		setTimeout(function () {
+			$(".form__login").css({
+				transform: 'rotateY(0deg)'
+			});
+		}, 320);	
+	});
+
+	$(".form__login_back").click(function(event) {
+		$(".main__button").fadeIn('300', function() {
+			
+		});
+
+		$(".form__login").css({
+			transition: 'transform .3s',
+			transform: 'rotateY(90deg)'
+		});
+
+		setTimeout(function(){
+			$(".form__login").css({
+				display: 'none'
+			});
+			$(".main__block").css({
+				display: 'block'
+			});
+		}, 300);
+
+		setTimeout(function () {
+			$(".main__block").css({
+				transform: 'rotateY(0deg)'
+			});
+		}, 320);
+	});
+}
+
+
+mainBlockApp();
+flipInit();
