1c62c68237ac8024628783769b8882debc0780a8
AlenaDmit <dmitalyona@yandex.ru>
2016-07-08 14:24:29 +0300

    script for yandex-maps

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,13 @@
+// function parallax(){
+// 	$(window).on('mousemove', function(e) {
+// 		var mouseX = e.pageX;
+// 		var mouseY = e.pageY;
+// 		var w = (window.innerWidth/2)-mouseX;
+// 		var h = (window.innerHeight/2)-mouseY;
+		
+// 	});
+// }
+
 function mainBlockApp() {	//main block vertical appearence
 	$(".main__block").css({
 		transform: 'rotateX(0deg)',
@@ -62,6 +72,38 @@ function flipInit(argument) { // main block flip
 			});
 		}, 320);
 	});
+}
+
+$(document).ready(function() {
+	createMap();
+});
+
+function createMap(){
+	ymaps.ready(init);
+    
+    function init(){
+		var myMap = new ymaps.Map("map", {
+        	center: [59.89106259461844, 30.450020485993274], 
+        	zoom: 12,
+        	controls: [],
+
+        });
+
+        myPlacemark = new ymaps.Placemark(
+        	[59.89666897024405, 30.392342263337017], 
+        	{ 
+        		hintContent: 'Москва!', 
+        		balloonContent: 'Столица России' 
+        	},{
+        		iconLayout: 'default#image',
+        		iconImageHref: 'images/marker.png',
+     			iconImageSize: [42, 58],
+        	});
+
+        myMap.geoObjects.add(myPlacemark);
+        myMap.controls.add('zoomControl');
+        myMap.behaviors.disable('scrollZoom');
+	}
 }
 
 
