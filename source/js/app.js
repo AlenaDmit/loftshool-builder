4ae3081b288be86f79195a6ccea1c5ad65b15a2e
AlenaDmit <dmitalyona@yandex.ru>
2016-07-11 00:37:05 +0300

    plugin scroll up/down added

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,50 @@
-// function parallax(){
-// 	$(window).on('mousemove', function(e) {
-// 		var mouseX = e.pageX;
-// 		var mouseY = e.pageY;
-// 		var w = (window.innerWidth/2)-mouseX;
-// 		var h = (window.innerHeight/2)-mouseY;
-		
-// 	});
-// }
+$(document).ready(function () {		// parallax
+
+  var layer = $('.parallax').find('.parallax__layer'); // Выбираем все parallax__layer в объект
+  // console.log(layer);
+  layer.map(function (key, value) { // Проходимся по всем элементам объекта
+    var bottomPosition = ((window.innerHeight / 2) * ((key + 1) / 100)); // Вычисляем на сколько нам надо опустить вниз наш слой что бы при перемещении по Y не видно было краев
+    var leftPosition = ((window.innerWidth / 2) * ((key + 1) / 100));
+    $(value).css({ // Выбираем элемент и добавляем css
+      'bottom': '-' + bottomPosition + 'px', // Выставляем bottom
+      'left': '-' + leftPosition + 'px',
+      'transform': 'translate3d(0px, 0px, 0)', // Подготавливаем browser к трансформации
+    });
+  });
+  
+  $(window).on('mousemove', function (e) { // Навешиваем событие перемещени мыши на window, первым аргументом в функцию-обработчик события отправляется ссылка на объект события
+    var mouse_dx = (e.pageX); // Узнаем положение мышки по X
+    var mouse_dy = (e.pageY); // Узнаем положение мышки по Y
+    // Т.к. мы делим экран на четыре части что бы в центре оказалась точка координат 0, то нам надо знать когда у нас будет -Х и +Х, -Y и +Y
+    var w = (window.innerWidth / 2) - mouse_dx; // Вычисляем для x перемещения
+    var h = (window.innerHeight / 2) - mouse_dy; // Вычисляем для y перемещения
+
+    layer.map(function (key, value) { // Проходимся по всем элементам объекта
+      var bottomPosition = ((window.innerHeight / 2) * ((key + 1) / 100)); // Вычисляем на сколько нам надо опустить вниз наш слой что бы при перемещении по Y не видно было краев
+      var widthPosition = w * ((key + 1) / 100); // Вычисляем коофицент смешения по X
+      var heightPosition = h * ((key + 1) / 100); // Вычисляем коофицент смешения по Ys
+
+      $(value).css({ // Выбираем элемент и добавляем css
+        'bottom': '-' + bottomPosition + 'px',  // Выставляем bottom
+        'transform': 'translate3d(' + widthPosition + 'px, ' + heightPosition + 'px, 0)', // Используем translate3d для более лучшего рендеринга на странице
+      });
+    });
+  });
+})
+
+function scroll(from,to){     // scroll to top/down
+	$(from).click(function (event) {
+			event.preventDefault();
+			var top = $(to).offset().top;
+			$('body,html').animate({scrollTop: top}, 600);
+		});
+};
+
+scroll($('.middle__button'), $('#slider'));
+scroll($('.form__top'), $('#header'));
+scroll($('.middle__button'), $('#slider'));
+
+
 
 function mainBlockApp() {	//main block vertical appearence
 	$(".main__block").css({
@@ -75,12 +113,13 @@ function flipInit(argument) { // main block flip
 }
 
 
-function initMap() {
+function initMap() {	// google map
 	var mapOptions = {
     zoom: 12,
     center: {lat: 59.9042161, lng: 30.1716925},
     disableDefaultUI: true,
     scrollwheel: false,
+    draggable: false,
     styles: [{"featureType":"administrative","elementType":"labels.text","stylers":[{"saturation":"-17"},{"lightness":"21"},{"gamma":"1.16"},{"weight":"2.35"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"gamma":"1"},{"lightness":"100"},{"color":"#ffffff"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#004d90"},{"visibility":"on"}]}]
 	}
  	
@@ -89,5 +128,5 @@ function initMap() {
 
 
 
-mainBlockApp();
+// mainBlockApp();
 flipInit();
